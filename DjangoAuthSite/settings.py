6875080744db977c0a38b87033d35be3f1f46a3e
AkbotaShaimerden <shaimerdenakbota9@gmail.com>
2025-05-20 21:53:11 +0500

    Миграции и статика собраны локально

--- DjangoAuthSite/settings.py
+++ DjangoAuthSite/settings.py
@@ -75,17 +75,30 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+from pathlib import Path
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+
+import dj_database_url
+import os
+
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
